aebd23b334e849173581629446592de3a14fbf8d
Nurgül Özçilingir <nurgulozcilingir@trair.com.tr>
2023-06-03 16:57:21 +0300

    added referances

--- src/components/Mentor/Mentor.tsx
+++ src/components/Mentor/Mentor.tsx
@@ -12,49 +12,73 @@ interface Product {
 const products: Product[] = [
     {
         id: 1,
-        name: 'Senior UX Designer',
+        name: 'Nusret',
         href: '#',
-        imageSrc: '/assets/mentor/boy1.svg',
+        imageSrc: '/assets/mentor/nusret.jpg',
         imageAlt: "Front of men's Basic Tee in black.",
         color: 'Shoo Thar Mein',
     },
     {
         id: 2,
-        name: 'Photoshop Instructor',
+        name: 'A101',
         href: '#',
-        imageSrc: '/assets/mentor/boy2.svg',
+        imageSrc: '/assets/mentor/a101.png',
         imageAlt: "Front of men's Basic Tee in black.",
         color: 'Cristian Doru Barin',
     },
     {
         id: 3,
-        name: 'SEO Expert',
+        name: 'Beymen',
         href: '#',
-        imageSrc: '/assets/mentor/boy3.svg',
+        imageSrc: '/assets/mentor/beymen.jpg',
         imageAlt: "Front of men's Basic Tee in black.",
         color: 'Tanzeel Ur Rehman',
     },
     {
         id: 4,
-        name: 'UI/UX Designer',
+        name: 'Netlog',
         href: '#',
-        imageSrc: '/assets/mentor/boy4.svg',
+        imageSrc: '/assets/mentor/netlog.jpg',
         imageAlt: "Front of men's Basic Tee in black.",
         color: 'Andrew Williams',
     },
     {
         id: 5,
-        name: 'Web Development / Web Design',
+        name: 'Dinamik',
         href: '#',
-        imageSrc: '/assets/mentor/boy5.svg',
+        imageSrc: '/assets/mentor/dinamik.jpg',
         imageAlt: "Front of men's Basic Tee in black.",
         color: 'Brad Schiff',
     },
     {
         id: 6,
-        name: 'Adobe Certified Instructor',
+        name: 'Defacto',
         href: '#',
-        imageSrc: '/assets/mentor/girl1.svg',
+        imageSrc: '/assets/mentor/defacto.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        color: 'Daniel Walter Scott',
+    },
+    {
+        id: 7,
+        name: 'Dolce',
+        href: '#',
+        imageSrc: '/assets/mentor/dolce.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        color: 'Daniel Walter Scott',
+    },
+    {
+        id: 7,
+        name: 'Koç',
+        href: '#',
+        imageSrc: '/assets/mentor/koc.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        color: 'Daniel Walter Scott',
+    },
+    {
+        id: 7,
+        name: 'Sendeo',
+        href: '#',
+        imageSrc: '/assets/mentor/sendeo.jpg',
         imageAlt: "Front of men's Basic Tee in black.",
         color: 'Daniel Walter Scott',
     },
@@ -65,18 +89,18 @@ const Mentor = () => {
             <div id="mentors-section" className="mx-auto max-w-2xl pb-16 px-4 sm:py-20 sm:px-6 lg:max-w-7xl lg:px-8">
 
                 <div className='sm:flex justify-between items-center mb-12'>
-                    <h2 className="text-3xl sm:text-5xl font-bold tracking-tight text-gray-900 my-4">Meet with our Mentors</h2>
+                    <h2 className="text-2xl sm:text-4xl font-bold tracking-tight text-gray-900 my-4">Referanslarımız</h2>
                     <div>
                         <button className="bg-transparent hover:bg-purple text-purple font-medium hover:text-white py-3 px-4 border border-lightgrey hover:border-transparent rounded">
-                            Explore 10+ our Mentor
+                            Tümünü Gör
                         </button>
                     </div>
                 </div>
 
-                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
                         <div key={product.id} className="group relative">
-                            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                            <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80 border border-[#d9d9d9]">
                                 <img
                                     src={product.imageSrc}
                                     alt={product.imageAlt}
@@ -93,7 +117,7 @@ const Mentor = () => {
                                             </a>
                                         </h3>
                                     </div>
-                                    <p className="mt-3 text-2xl font-semibold text-offblack text-center">{product.color}</p>
+                                    <p className="mt-3 text-2xl font-semibold text-white text-center">{product.color}</p>
                                 </div>
                             </div>
                         </div>
